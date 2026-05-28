352b234bfa7641de18e875a77e7ebd3fa198f6cd
vsakhno <vsakhno@oggettoweb.com>
2026-05-28 13:02:01 +0100

    fix git e2e test

--- tests/e2e/pages/site_create_page.py
+++ tests/e2e/pages/site_create_page.py
@@ -8,12 +8,21 @@ from tests.e2e.pages.admin_base_page import AdminBasePage
 class SiteCreatePage(AdminBasePage):
     def __init__(self, page: Page) -> None:
         super().__init__(page)
+        self.github_repository_button: Locator = page.get_by_role(
+            "button",
+            name=re.compile(r"github\s+repository", re.IGNORECASE),
+        ).first
         self.git_repo_input: Locator = page.get_by_placeholder(re.compile("github", re.IGNORECASE)).first
         self.create_button: Locator = page.get_by_role("button", name=re.compile("create", re.IGNORECASE)).first
 
+    def choose_github_repository(self) -> None:
+        expect(self.github_repository_button).to_be_visible(timeout=20_000)
+        self.github_repository_button.click()
+
     def create_from_git(self, repo_url: str) -> None:
+        self.choose_github_repository()
         expect(self.git_repo_input).to_be_visible(timeout=20_000)
         self.git_repo_input.fill(repo_url)
-        
+
         expect(self.create_button).to_be_enabled()
         self.create_button.click()
